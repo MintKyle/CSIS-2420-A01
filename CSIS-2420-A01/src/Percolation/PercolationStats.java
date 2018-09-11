6e3d9f378bef3bb4ab750cf4943d96332fc65d81
Kyle <Kyle@Kyle.hsd1.ut.comcast.net>
2018-09-11 15:50:19 -0600

    First Commit

---
+++ CSIS-2420-A01/src/Percolation/PercolationStats.java
@@ -0,0 +1,30 @@
+package Percolation;
+
+public class PercolationStats {
+	public PercolationStats(int N, int T) {
+		// perform T independent experiments on an N­by­N grid
+		
+		
+		
+	}
+
+	public double mean() {
+		return 0;
+		// sample mean of percolation threshold
+	}
+
+	public double stddev() {
+		return 0;
+		// sample standard deviation of percolation threshold
+	}
+
+	public double confidenceLow() {
+		return 0;
+		// low endpoint of 95% confidence interval
+	}
+
+	public double confidenceHigh() {
+		return 0;
+		// high endpoint of 95% confidence interval
+	}
+}
