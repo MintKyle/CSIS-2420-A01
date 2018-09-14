c743a0b2df669d44d8bf1c3c55fe60b1a8d4cb1f
whitney smith <whitney.smith@tealdrones.com>
2018-09-14 10:56:40 -0600

    Main added for testing, PercolationStats() needs to be finished

--- CSIS-2420-A01/src/Percolation/PercolationStats.java
+++ CSIS-2420-A01/src/Percolation/PercolationStats.java
@@ -1,32 +1,52 @@
 // Kyle Fackrell, Whitney Smith
-
 package Percolation;
 
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
+
 public class PercolationStats {
+
+	int experiments;
+	double[] openedAverages;
+
 	public PercolationStats(int N, int T) {
-		// perform T independent experiments on an N­by­N grid
-		
-		
-		
+		// perform T independent experiments on a grid
+		if (N < 0 || T < 0) {
+			throw new IllegalArgumentException("Error: Negative number.");
+		}
+		experiments = T;
+		//openedAverages = runExperiment(N, T);
 	}
 
 	public double mean() {
-		return 0;
 		// sample mean of percolation threshold
+		return StdStats.mean(openedAverages);
 	}
 
 	public double stddev() {
-		return 0;
 		// sample standard deviation of percolation threshold
+		return StdStats.stddev(openedAverages);
 	}
 
 	public double confidenceLow() {
-		return 0;
 		// low endpoint of 95% confidence interval
+		return mean() - StdStats.stddev(openedAverages);
 	}
 
 	public double confidenceHigh() {
-		return 0;
 		// high endpoint of 95% confidence interval
+		return mean() + StdStats.stddev(openedAverages);
+	}
+
+	// a main() to test the PercolationStats class
+	public static void main(String[] args) {
+
+		PercolationStats test = new PercolationStats(200, 100);
+
+		System.out.println("Results:\n");
+		StdOut.println("Mean: " + test.mean());
+		StdOut.println("Standard Deviation: " + test.stddev());
+		StdOut.println("Confidence Low: " + test.confidenceLow());
+		StdOut.println("Confidence High: " + test.confidenceHigh());
 	}
 }
